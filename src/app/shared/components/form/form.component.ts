792701ae73ba6842a7aecab3856e9150e635f7d6
Nathan-Levi1701 <nathan.levi@epiuse.com>
2023-11-20 08:33:20 +0200

    Fixed unit type selection error

--- src/app/shared/components/form/form.component.ts
+++ src/app/shared/components/form/form.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {FormLogicService} from "../../services/form-logic.service";
-import {FormArray, FormBuilder, FormControl, FormGroup} from "@angular/forms";
+import {FormBuilder, FormGroup} from "@angular/forms";
 
 
 @Component({
@@ -15,19 +15,16 @@ export class FormComponent implements OnInit{
   constructor(public formLogicService:FormLogicService, public formBuilder: FormBuilder) {
     this.formGroup = formBuilder.group({
       conversionType: ['imperial'],
-      convertFrom: this.formBuilder.array([])
+      convertFrom: ['']
     });
   }
   ngOnInit(): void {
-    this.formLogicService.buildForm(this.formGroup.get("conversionType").value);
-    this.formGroup.get('conversionType').valueChanges.subscribe((value) => {
-      console.log(value);
-      this.listItems = this.formLogicService.buildForm(value.conversionType);
-      this.listItems.forEach((field: string) => {
-        (this.formGroup.get('convertFrom') as FormArray).push(new FormControl(field))
-      });
-      this.listItems = this.formGroup.get('convertFrom').value;
-    });
+    this.listItems = this.formLogicService.buildForm(this.formGroup.get("conversionType").value);
 
+    this.formGroup.get('conversionType').valueChanges.subscribe((value) => {
+      this.listItems = this.formLogicService.buildForm(value);
+      console.log(this.listItems)
+      this.formGroup.get('convertFrom').setValue(this.listItems)
+    });
   }
 }
--- src/app/shared/components/form/form.module.ts
+++ src/app/shared/components/form/form.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {FormComponent} from "./form.component";
 import {MaterialModule} from "../../../modules/material.module";
-import {ReactiveFormsModule} from "@angular/forms";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 
 
@@ -11,7 +11,8 @@ import {ReactiveFormsModule} from "@angular/forms";
   imports: [
     CommonModule,
     MaterialModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    FormsModule
   ],
   exports: [FormComponent]
 })
